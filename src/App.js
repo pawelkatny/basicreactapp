1d0b57e06863d3c028334d63a70f6294e9fefd42
pawelkatny <pawel.katny@gmail.com>
2020-02-25 00:17:27 +0100

    24/02/2020 Adds updated basic React app - a practice for working with components, state, dynamically rendered elements and CSS modules.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,76 @@
 import React, { Component } from 'react';
-import './App.css';
 import Person from './Person/Person';
+import styles from './App.module.css'
+import ErrorHandler from './ErrorHandler/ErrorHandler';
 
 
 class App extends Component {
+  state = {
+    persons: [
+      { id: 1, name: 'Max', age: 60 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Pablo', age: 15 }
+    ],
+    showPersons: false
+  };
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(prsn => {
+      return prsn.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({ persons:persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
   render() {
-    return (
-      <div className="App">
+
+    let person = null;
+    let btnClass = [styles.ButtonApp];
+
+    if (this.state.showPersons) {
+      person = (
+
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangeHandler(event, person.id)}/>
+          })}
+        </div>
+      );
+
+        btnClass.push(styles.Black);
+    }
+
+      return (
+      <div className={styles.App}>
         <h1>Hello, I am React App</h1>
-        <Person />
+        <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>Toggle Persons List</button>
+        {person}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import styles from'./Person.module.css';
 
-const person = () => {
+const person = (props) => {
+
     return (
-        <p>I am a person!</p>
+
+        <div className={styles.Person}>
+            <p  onClick={props.click}>Hi, my name is {props.name} and I am {props.age}. </p>
+            <p>You can change my name if you wish!</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
     );
 }
 
